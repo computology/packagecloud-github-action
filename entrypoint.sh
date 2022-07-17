1f27343728cb6d3d8e872789103e970247d83dbe
nethsix <nethsix@gmail.com>
2022-07-17 17:46:18 +0900

    Use rvm before calling package_cloud cli

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,4 @@ UPLOAD_PATH="${PACKAGECLOUD_USERNAME}/${PACKAGECLOUD_REPONAME}"
 echo "Pushing package: ${PACKAGE_NAME}"
 echo "To repository: ${UPLOAD_PATH}"
 
-package_cloud push ${UPLOAD_PATH} ${PACKAGE_NAME}
+rvm use 2.7.5 && package_cloud push ${UPLOAD_PATH} ${PACKAGE_NAME}
