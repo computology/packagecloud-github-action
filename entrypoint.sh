36c7dc09dcb42226b1ebb43964347eda7f564066
nethsix <nethsix@gmail.com>
2022-07-17 17:51:34 +0900

    source rvm before using in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,5 @@ UPLOAD_PATH="${PACKAGECLOUD_USERNAME}/${PACKAGECLOUD_REPONAME}"
 echo "Pushing package: ${PACKAGE_NAME}"
 echo "To repository: ${UPLOAD_PATH}"
 
+source /usr/local/rvm/scripts/rvm
 rvm use 2.7.5 && package_cloud push ${UPLOAD_PATH} ${PACKAGE_NAME}
